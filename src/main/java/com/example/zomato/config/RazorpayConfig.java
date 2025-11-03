0926673585ecf50489491ce734df18a0d9bd7c8b
webcreator30 <hustel475@gmail.com>
2025-11-03 21:20:55 +0530

    Razorpay Configuration Set Up

---
+++ src/main/java/com/example/zomato/config/RazorpayConfig.java
@@ -0,0 +1,26 @@
+package com.example.zomato.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+
+@Configuration
+public class RazorpayConfig {
+ 
+    
+    @Value("${api_key}")
+    private String apiKey;
+    
+    @Value("${key_secret}")
+    private String keySecret;
+
+    // RazorpayClient client is the official Razorpay SDK class that enables
+    // your app to interact with Razorpay APIs (e.g., create orders, capture payments).
+    @Bean
+    public RazorpayClient razorpayClient() throws RazorpayException{
+        return new RazorpayClient(apiKey, keySecret);
+    }
+}
