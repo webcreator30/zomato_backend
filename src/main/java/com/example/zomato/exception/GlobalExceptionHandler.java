847a48f210f37593908fe24a9cbf13423863fb88
webcreator30 <hustel475@gmail.com>
2025-11-09 18:14:45 +0530

    Global Exception Handler Added

---
+++ src/main/java/com/example/zomato/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.zomato.exception;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalState(IllegalStateException ex) {
+        Map<String, Object> res = new HashMap<>();
+        res.put("timestamp", LocalDateTime.now());
+        res.put("error", ex.getMessage());
+        res.put("status", HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(res, HttpStatus.BAD_GATEWAY);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAll(Exception ex) {
+        Map<String, Object> res = new HashMap<>();
+        res.put("timestamp", LocalDateTime.now());
+        res.put("error", ex.getMessage());
+        res.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+}
